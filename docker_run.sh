3c46d711e46a1ea404678504df82a4d7ebc16c9f
Radlantic <radlanticza@gmail.com>
2025-02-14 14:26:44 +0200

    update bash script

--- docker_run.sh
+++ docker_run.sh
@@ -3,9 +3,9 @@ sudo docker build . -t spreadsheet_server
 sudo docker stop spreadsheet_server
 sudo docker rm spreadsheet_server
 sudo docker run -d \
-  -v ./spreadsheets/:/home/libreuser/spreadsheet_server/spreadsheets \
-  -v ./saved_spreadsheets/:/home/libreuser/spreadsheet_server/saved_spreadsheets \
-  -v ./log/:/home/libreuser/spreadsheet_server/log \
+  -v ./spreadsheets/:/home/ubuntu/spreadsheet_server/spreadsheets \
+  -v ./saved_spreadsheets/:/home/ubuntu/spreadsheet_server/saved_spreadsheets \
+  -v ./log/:/home/ubuntu/spreadsheet_server/log \
   --restart always \
   -p 5555:5555 \
   --name spreadsheet_server spreadsheet_server
